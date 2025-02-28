54fc74680d8b79c00a0cc0ac287b72e891bf0822
pablito <lukegassson@gmail.com>
2025-02-28 14:11:45 +1300

    add bedrock bucket

---
+++ tf/module_bedrock_bucket.tf
@@ -0,0 +1,14 @@
+module "lambda_zips_bucket" {
+  source = "./modules/s3"
+
+  bucket         = "metrosafety-bedrock-output-${var.env}-${var.namespace}"
+  env            = var.env
+  encrypt_bucket = false
+
+  versioning = {
+    enabled    = "Enabled"
+    mfa_delete = "Disabled"
+  }
+
+  force_destroy = true
+}
