b448df4641ba74258ab6de6b83e99aa96c87ddb6
pablito <lukegassson@gmail.com>
2025-03-04 15:50:06 +1300

    final fix hopefully

--- tf/lambda_layer.tf
+++ tf/lambda_layer.tf
@@ -1,3 +1,20 @@
+resource "aws_iam_role" "lambda_role" {
+  name               = "bedrock_lambda_execution_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+
 resource "aws_iam_policy" "bedrock_invoke_policy" {
   name        = "BedrockInvokePolicy"
   description = "Allows AWS Lambda to call AWS Bedrock models"
