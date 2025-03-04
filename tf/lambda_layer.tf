82fcc098a1bba95e1c77b75f58393a6b47d69299
pablito <lukegassson@gmail.com>
2025-03-04 15:21:58 +1300

    testing w perms

--- tf/lambda_layer.tf
+++ tf/lambda_layer.tf
@@ -1,20 +1,18 @@
-resource "aws_iam_policy" "bedrock_invoke_policy" {
-  name        = "BedrockInvokePolicy"
-  description = "Allows AWS Lambda to call AWS Bedrock models"
-
-  policy = jsonencode({
+resource "aws_iam_role" "lambda_role" {
+  name               = "bedrock_lambda_execution_role"
+  assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = "bedrock:InvokeModel"
-        Resource = "*"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
       }
-    ]
+    }]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_bedrock_attach" {
   role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.bedrock_invoke_policy.arn
+  policy_arn = "arn:aws:iam::aws:policy/AmazonBedrockFullAccess"  # Use AWS managed policy
 }
