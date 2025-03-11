26d7814062ef9a630f829057b6b490ee3e2ab945
pablito <lukegassson@gmail.com>
2025-03-11 14:46:29 +1300

    perms

--- tf/lambda_layer.tf
+++ tf/lambda_layer.tf
@@ -35,3 +35,31 @@ resource "aws_iam_role_policy_attachment" "lambda_bedrock_attach" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.bedrock_invoke_policy.arn
 }
+
+resource "aws_iam_policy" "lambda_s3_access" {
+  name        = "LambdaS3WriteAccess"
+  description = "Allows Lambda to write proofing data to S3"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket"
+        ],
+        Resource = [
+          "arn:aws:s3:::metrosafety-bedrock-output-data-dev-bedrock-lambda",
+          "arn:aws:s3:::metrosafety-bedrock-output-data-dev-bedrock-lambda/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3" {
+  policy_arn = aws_iam_policy.lambda_s3_access.arn
+  role       = aws_iam_role.lambda_exec_role.name
+}
