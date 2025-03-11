106d45ee03b9675275ae64e3aa9861b06eb0f8e6
pablito <lukegassson@gmail.com>
2025-03-11 14:56:10 +1300

    allowing iam for metadta

--- tf/dynamodb.tf
+++ tf/dynamodb.tf
@@ -14,3 +14,31 @@ resource "aws_dynamodb_table" "proofing_metadata" {
     Environment = var.env
   }
 }
+
+resource "aws_iam_policy" "lambda_dynamodb_access" {
+  name        = "LambdaDynamoDBAccess"
+  description = "Allows Lambda to write to the ProofingMetadata DynamoDB table"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:Scan",
+          "dynamodb:Query"
+        ],
+        Resource = "arn:aws:dynamodb:eu-west-2:837329614132:table/ProofingMetadata"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb" {
+  policy_arn = aws_iam_policy.lambda_dynamodb_access.arn
+  role       = "bedrock-lambda-salesforce_input"
+}
