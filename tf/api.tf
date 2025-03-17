f9c3457f2ad3baeb21e0f14956cd61e74363a223
pablito <lukegassson@gmail.com>
2025-03-17 15:08:14 +1300

    creating log group jsut for api

--- tf/api.tf
+++ tf/api.tf
@@ -21,6 +21,34 @@ resource "aws_apigatewayv2_stage" "lambda_stage" {
   api_id      = aws_apigatewayv2_api.lambda_api.id
   name        = "prod"
   auto_deploy = true
+
+  default_route_settings {
+    logging_level = "INFO"
+    data_trace_enabled = true
+    throttling_burst_limit = 100
+    throttling_rate_limit  = 50
+  }
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gw_logs.arn
+    format = jsonencode({
+      requestId       = "$context.requestId"
+      sourceIp        = "$context.identity.sourceIp"
+      requestTime     = "$context.requestTime"
+      protocol        = "$context.protocol"
+      httpMethod      = "$context.httpMethod"
+      resourcePath    = "$context.resourcePath"
+      routeKey        = "$context.routeKey"
+      status          = "$context.status"
+      responseLength  = "$context.responseLength"
+      integrationError = "$context.integrationErrorMessage"
+    })
+  }
+}
+
+resource "aws_cloudwatch_log_group" "api_gw_logs" {
+  name = "/aws/api_gw/proofing"
+  retention_in_days = 7
 }
 
 #perms
