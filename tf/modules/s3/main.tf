582a1d924c8cf1f382e424b37ca63dea26d73135
pablito <lukegassson@gmail.com>
2025-04-10 15:58:12 +1200

    policy

--- tf/modules/s3/main.tf
+++ tf/modules/s3/main.tf
@@ -217,3 +217,29 @@ resource "aws_s3_object" "create_folders" {
   bucket = aws_s3_bucket.this.id
   key    = "${each.value}/"  # creats empty folders
 }
+
+data "aws_iam_policy_document" "lambda_s3_read" {
+  statement {
+    sid    = "AllowLambdaS3Read"
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:HeadObject",
+      "s3:ListBucket"
+    ]
+    resources = [
+      "arn:aws:s3:::metrosafetyprodfiles",
+      "arn:aws:s3:::metrosafetyprodfiles/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "lambda_s3_read_policy" {
+  name   = "LambdaS3ReadPolicy"
+  policy = data.aws_iam_policy_document.lambda_s3_read.json
+}
+
+resource "aws_iam_role_policy_attachment" "attach_lambda_s3_read" {
+  role       = aws_iam_role.lambda_role.name  # or your appropriate Lambda role resource
+  policy_arn = aws_iam_policy.lambda_s3_read_policy.arn
+}
