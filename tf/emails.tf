fe54ddac7c3c398e5fa79d219d5825918471726c
pablito <lukegassson@gmail.com>
2025-06-09 19:30:16 +1200

    email ses setup

--- tf/emails.tf
+++ tf/emails.tf
@@ -1,5 +1,4 @@
 # 1. (Optional) Verify your "From" address in SES
-#    — only needed if you haven’t already done this in Terraform:
 resource "aws_ses_email_identity" "sender" {
   email = "luke.gasson@metrosafety.co.uk"
 }
@@ -8,12 +7,17 @@ resource "aws_ses_email_identity" "recipient" {
   email = "luke.gasson@metrosafety.co.uk"
 }
 
-# 2. Look up the Lambda’s execution role by name
+# 2. Look up the Salesforce‐input Lambda role (you already have this)
 data "aws_iam_role" "salesforce_input" {
   name = "bedrock-lambda-salesforce_input"
 }
 
-# 3. Define a policy that allows SendEmail (and SendRawEmail if you need it)
+# 2b. Look up the checklist_proofing Lambda role 
+data "aws_iam_role" "checklist_proofing" {
+  name = "bedrock-lambda-checklist_proofing"
+}
+
+# 3. Define a policy that allows SendEmail (and SendRawEmail)
 resource "aws_iam_policy" "ses_send_email" {
   name        = "bedrock-lambda-ses-policy"
   description = "Allow only sending from our verified address"
@@ -24,14 +28,22 @@ resource "aws_iam_policy" "ses_send_email" {
       Sid      = "AllowSendEmailFromMyAddress",
       Effect   = "Allow",
       Action   = ["ses:SendEmail","ses:SendRawEmail"],
-      Resource = "arn:aws:ses:eu-west-2:837329614132:identity/luke.gasson@metrosafety.co.uk"
+      Resource = "arn:aws:ses:eu-west-2:${data.aws_caller_identity.current.account_id}:identity/luke.gasson@metrosafety.co.uk"
     }]
   })
 }
 
-
-# 4. Attach that policy to the Lambda role
+# 4. Attach that policy to the Salesforce‐input role (you already have this)
 resource "aws_iam_role_policy_attachment" "salesforce_input_ses" {
   role       = data.aws_iam_role.salesforce_input.name
   policy_arn = aws_iam_policy.ses_send_email.arn
 }
+
+# 4b. Attach the same policy to the checklist_proofing role (new)
+resource "aws_iam_role_policy_attachment" "checklist_proofing_ses" {
+  role       = data.aws_iam_role.checklist_proofing.name
+  policy_arn = aws_iam_policy.ses_send_email.arn
+}
+
+# (Optional) to avoid hard-coding the account ID, you can add:
+data "aws_caller_identity" "current" {}
