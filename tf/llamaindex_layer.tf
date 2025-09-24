9756d4d08fafaf619443a23d1c3893d8c6a7d75f
pablito <lukegassson@gmail.com>
2025-09-24 17:12:04 +1200

    trying

--- tf/llamaindex_layer.tf
+++ tf/llamaindex_layer.tf
@@ -1,7 +1,10 @@
 resource "aws_lambda_layer_version" "llamaindex" {
-  layer_name               = "llama-py-313-x86_64"
+  layer_name               = "llama-py-312-x86_64"   # rename for clarity
   s3_bucket                = module.lambda_zips_bucket.this_s3_bucket_id
   s3_key                   = "layers/llama-layer.zip"
   compatible_runtimes      = ["python3.12"]
   compatible_architectures = ["x86_64"]
+
+  # force a new layer version whenever you upload a new zip
+  description = "llama layer build ${timestamp()}"
 }
